d912b2905c14eccb339b03f97c506dd472bb8d7d
walid moussa <walidmoussa00@gmail.com>
2025-06-26 23:37:20 +0200

    added some styling tweeks

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,12 +1,12 @@
 import AllPosts from "../../../components/AllPosts/AllPosts";
 import Footer from "../../../components/Footer/Footer";
 import Navbar from "../../../components/Navbar/Navbar";
-
+import "./page.css";
 export default function Posts() {
   return (
-    <div>
-      <h1 className="EnormousPageTitle">THE BLOG</h1>
+    <div className="PageContainer">
       <Navbar />
+      <h1 className="EnormousPageTitle">THE BLOG</h1>
       <AllPosts />
       <Footer />
     </div>
