7b6257d5f529b55de4ae78f10784847e52a4199c
walid moussa <walidmoussa00@gmail.com>
2025-06-26 23:48:25 +0200

    did navbar without dark mode toggle and functionality

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,3 +1,21 @@
+"use client";
+
+import "./Navbar.css";
+import { useState } from "react";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <nav className="Navbar">
+      <div className="NavbarName">Walid Moussa</div>
+      <button className="NavbarToggle" onClick={toggleDarkMode}>
+        {darkMode ? "Light Mode" : "Dark Mode"}
+      </button>
+    </nav>
+  );
 }
